897e7d1aac814c85124a310bbfab4572da970a35
Sven <glaglasven@live.com>
2017-01-13 09:55:35 +0100

    Correction to coffee

--- Coffee/MainPage.xaml.cs
+++ Coffee/MainPage.xaml.cs
@@ -12,6 +12,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Toolkit.Uwp.UI.Animations;
+using System.Collections.ObjectModel;
+using Microsoft.Toolkit.Uwp;
+using System.Diagnostics;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +26,143 @@ namespace Coffee
     /// </summary>
     public sealed partial class MainPage : Page
     {
+		public ObservableCollection<People> ListPeople { get; set; }
+		public People Selected { get; set; }
+		private string key = "Save";
+		private bool locker = false;
+		private string newName = "";
+
         public MainPage()
         {
-            this.InitializeComponent();
-        }
-    }
+			ListPeople = new ObservableCollection<People>();
+			Selected = new People();
+			this.InitializeComponent();
+			GetDataAsync();
+			this.Bindings.Update();
+			if (combo.Items.Count > 0)
+			{
+				Selected = ListPeople[0];
+				combo.SelectedIndex = 0;
+			}
+			this.Bindings.Update();
+		}
+
+		private async void GetDataAsync()
+		{
+			var helper = new LocalObjectStorageHelper();
+			if (await helper.FileExistsAsync(key))
+			{
+				Debug.WriteLine("Loaded data");
+				ListPeople = await helper.ReadFileAsync<ObservableCollection<People>>(key);
+			}
+			else
+			{
+				ListPeople.Add(new People("Morgane", 2, 0, 0));
+				SaveData();
+			}
+		}
+
+		private async void SaveData()
+		{
+			if (locker == true)
+					return;
+			locker = true;
+			var helper = new LocalObjectStorageHelper();
+			await helper.SaveFileAsync(key, ListPeople);
+			Debug.WriteLine("Saved");
+			locker = false;
+		}
+
+		private async void backgroundImage_Loaded(object sender, RoutedEventArgs e)
+		{
+			foreach (var child in MainStack.Children)
+			{
+				if (child.GetType() == typeof(StackPanel))
+				{
+					foreach (var subchild in (child as StackPanel).Children)
+					{
+						if (subchild.GetType() != typeof(StackPanel))
+							subchild.Fade(0, 0, 0).Start();
+						else
+							subchild.Fade(0, 0, 0).Start();
+						Debug.WriteLine("Done fade subchild");
+					}
+				}
+				else
+					child.Fade(0, 0, 0).Start();
+			}
+			await backgroundImage.Blur(8f, 1000, 300).StartAsync();
+			int delay = 400;
+			int total = 0;
+			foreach (var child in MainStack.Children)
+			{
+				if (child.GetType() == typeof(StackPanel))
+				{
+					foreach (var subchild in (child as StackPanel).Children)
+					{
+						if (subchild.GetType() != typeof(StackPanel))
+							subchild.Fade(1, 500, delay * total++).Start();
+						else
+							subchild.Fade(1, 500, delay * total++).Start();
+						Debug.WriteLine("Done subchild");
+					}
+				}
+					child.Fade(1, 500, delay * total++).Start();
+			}
+		}
+
+		private void ButtonCoffee_Click(object sender, RoutedEventArgs e)
+		{
+			int n = combo.SelectedIndex;
+			if (n < 0)
+				return;
+			ListPeople[n].AddCoffee(sucreBox.IsChecked, CremeBox.IsChecked);
+			Selected = ListPeople[n];
+			SaveData();
+			this.Bindings.Update();
+		}
+
+		private void combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (combo.SelectedIndex < 0)
+				return;
+			Selected = ListPeople[combo.SelectedIndex];
+			this.Bindings.Update();
+		}
+
+		private async void AppBarButton_ClickAsync(object sender, RoutedEventArgs e)
+		{
+			TextBox t = new TextBox();
+			t.PlaceholderText = "Nom";
+			t.TextChanged += (s, i) => { newName = t.Text; };
+			ContentDialog contentDialog = new ContentDialog() { Content = t, Title = "Ajouter un buveur de feca" };
+			contentDialog.MinWidth = this.ActualWidth;
+			contentDialog.MaxWidth = this.ActualWidth;
+			contentDialog.PrimaryButtonText = "Ajouter";
+			contentDialog.SecondaryButtonText = "Annuler";
+			contentDialog.SecondaryButtonClick += (obj, ex) => { newName = ""; };
+			contentDialog.PrimaryButtonClick += (obj, ex) => { ListPeople.Add(new People(newName, 0, 0, 0)); SaveData(); };
+			await contentDialog.ShowAsync();
+		}
+
+		private async void Button_Click(object sender, RoutedEventArgs e)
+		{
+			string _name = (sender as Button).Tag.ToString();
+			People current = null;
+			foreach (People item in ListPeople)
+				if (item.Name == _name)
+					current = item as People;
+			if (current == null)
+				return;
+			ContentDialog content = new ContentDialog() { Title = "Valider la suppression", Content = "Supprimer " + _name + "?" };
+			content.MinWidth = this.ActualWidth;
+			content.MaxWidth = this.ActualWidth;
+			content.PrimaryButtonText = "Supprimer";
+			content.SecondaryButtonText = "Non!";
+			content.SecondaryButtonClick += (ob, ex) => { };
+			content.PrimaryButtonClick += (ob, ex) => { ListPeople.Remove(current); SaveData(); };
+			await content.ShowAsync();
+
+		}
+	}
 }
---
+++ Coffee/People.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffee
+{
+	public class People
+	{
+		public string Name { get; set; }
+		public int NbCoffee { get; set; }
+		public int NbAddons { get; set; }
+		public double Money { get; set; }
+		private double PriceCoffee = 1.60;
+		private double Addon = 0.1;
+
+		public People()
+		{
+
+		}
+		public People(string _name, int _nCoffee, int _nAddons, double _money)
+		{
+			Name = _name; NbCoffee = _nCoffee; NbAddons = _nAddons; Money = _money;
+			if (Money == 0)
+				ComputeMoney();
+		}
+
+		private void ComputeMoney()
+		{
+			Money = NbCoffee * PriceCoffee + NbAddons * Addon;
+		}
+
+		public void AddCoffee(bool? Sugar, bool? Cream)
+		{
+			NbCoffee++;
+			Money += PriceCoffee;
+			if(Cream == true)
+			{
+				NbAddons++; Money += Addon;
+			}
+			if (Sugar == true)
+			{
+				NbAddons++; Money += Addon;
+			}
+		}
+
+	}
+}
